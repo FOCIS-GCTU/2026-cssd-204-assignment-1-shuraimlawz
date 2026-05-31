8a6ad604ae7546a57efb0cda9182946c97da5298
Shuraim Issaka <2425404260@live.gctu.edu.gh>
2026-05-31 18:05:05 +0000

    Submit assignment: 1, Scripting Language

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+#File: initials.py
+#Description: Prints styled large initials SIS with required spacing and block periods.
+#Assignment Number: 1
+#Name: Shuraim Issaka
+#STUDENT ID: 2425404260
+#Email: 2425404260@live.gctu.edu.gh
+#Grader: Carolyn
+#
+#On my honor, SHuraim Issaka, this programming is my own work
+#and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This function prints the small initials and stylized large block initials.
+    print()
+    print("...SIS")
+    print()
+    print("....SSSSSSSSSS.........IIIIIIIIIIII.........SSSSSSSSSS.....")
+    print("...SSSSSSSSSSSS........IIIIIIIIIIII........SSSSSSSSSSSS....")
+    print("...SSSS....................IIII............SSSS............")
+    print("....SSSSS..................IIII.............SSSSS...........")
+    print(".....SSSSSSSS..............IIII..............SSSSSSSS......")
+    print(".......SSSSSSSS............IIII................SSSSSSSS....")
+    print("...........SSSSS...........IIII....................SSSSS...")
+    print("............SSSS...........IIII.....................SSSS...")
+    print("...SSSSSSSSSSSS...**.......IIII.......**...SSSSSSSSSSSS...**")
+    print("....SSSSSSSSSS....**...IIIIIIIIIIII...**....SSSSSSSSSS....**")
+    print()
 
 
 main()
